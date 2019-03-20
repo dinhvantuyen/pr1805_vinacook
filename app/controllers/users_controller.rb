b1ce1075acfbb8fef981ba967198e41d1fb7a422
dinhvantuyen <dinhtuyen3795@gmail.com>
2019-03-20 20:10:18 +0700

    registered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,30 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find params[:id]
+    @user = User.find_by id: params[:id]
   end
 
-  def edit; end
+  def new
+    @user = User.new
+  end
 
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Chào mừng bạn đã đến wwebsite VINACOOK!"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :address, :email, :phone, :password
+  end
 end
