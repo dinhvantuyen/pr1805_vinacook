0a8d2a593e66b53cd0e024aa790c8ac03b35f106
dinhvantuyen <dinhtuyen3795@gmail.com>
2019-05-08 14:52:46 +0700

    rivew product admin

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
   mount_uploader :image, PictureUploader
+  enum status:  [:Conhang, :hethang]
+
+  validates :name, presence:true, uniqueness: true
+  default_scope -> { order(created_at: :desc)}
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ },
+  numericality: { greater_than: 0, less_than: 1000000}
+  validates :sale_price, presence: true, allow_blank: true, format: { with: /\A\d+(?:\.\d{2})?\z/ },
+  numericality: { greater_than: 0, less_than: 1000000}
 end
